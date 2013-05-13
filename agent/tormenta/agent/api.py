cd213cc316a3bec15948fe0f71e5d74bb822ee0e
Jorge Niedbalski R <niedbalski@gmail.com>
2013-05-13 18:29:36 -0300

    [all] pep8 cleanup

--- agent/tormenta/agent/api.py
+++ agent/tormenta/agent/api.py
@@ -4,17 +4,15 @@ __author__ = 'Jorge Niedbalski R. <jnr@pyrosome.org>'
 
 from tormenta.core.config import settings
 from tormenta.core.decorators import has_access_token
-from tormenta.core.model import ( db, Token, PublicKey, 
-                                  Instance,  
-                                  InstanceResource as InstanceResourceModel)
-
+from tormenta.core.model import (db, Token, PublicKey,
+                                 Instance,
+                                 InstanceResource as InstanceResourceModel)
 from tormenta.agent import (initialize, params)
 
 from flask import Flask, request
-from flask.ext.restful import ( Resource, Api, 
-                                marshal_with, marshal, abort )
+from flask.ext.restfuThe cl import (Resource, Api,
+                                    marshal_with, marshal, abort)
 import logging
-
 
 logger = logging.getLogger('tormenta.agent.api')
 
@@ -22,12 +20,12 @@ logger = logging.getLogger('tormenta.agent.api')
 app = Flask(__name__)
 api = Api(app, prefix='/api/v%d' % settings.options.api_version)
 
-#agent = initialize()
 
 def marshal_and_count(n, r, f):
     if not isinstance(r, list):
         r = [r]
     r = map(lambda q: marshal(q, f), r)
+
     return dict({'count': len(r), '%s' % n: r})
 
 
@@ -44,7 +42,7 @@ class TokenResource(Resource):
 
 class PublicKeyResource(Resource):
 
-    method_decorators = [ has_access_token ]
+    method_decorators = [has_access_token]
     params = params.PublicKey()
 
     def get(self, *args, **kwargs):
@@ -59,7 +57,7 @@ class PublicKeyResource(Resource):
 
     def post(self, *args, **kwargs):
         (args, token) = (self.params.post, kwargs.get('token'))
-        
+
         pkey_param = args['public_key']
         pkey_id = PublicKey.encode(token, pkey_param)
 
@@ -67,18 +65,17 @@ class PublicKeyResource(Resource):
             key = PublicKey.select().where(PublicKey.token == token).where(
                 PublicKey.public_key_id == pkey_id).get()
         except:
-            key = PublicKey.create(public_key_id=pkey_id, 
-                                   raw=pkey_param, 
-                                   token=token)
+            key = PublicKey.create(public_key_id=pkey_id,
+                                   raw=pkey_param, token=token)
 
         return marshal_and_count('keys', key, self.params.fields)
 
 
 class InstanceResource(Resource):
-    method_decorators = [ has_access_token ]
+    method_decorators = [has_access_token]
     params = params.Instance()
 
-    def get(self, *args, **kwargs):    
+    def get(self, *args, **kwargs):
         """
            List all instances
         """
@@ -97,43 +94,40 @@ class InstanceResource(Resource):
         for instance in instances:
             resource_filters = args.get('resource_filter', None)
             if resource_filters:
-                if instance.has_resources(resource_filters) == True:
+                if instance.has_resources(resource_filters):
                     m_instances.append(instance)
             else:
                 m_instances.append(instance)
 
-        m_instances = map(lambda i: i.hydrate(marshal, 
-                                              self.params.fields, 
-                                              self.params.resource.fields), 
-                                              m_instances)
+        m_instances = map(lambda i: i.hydrate(marshal,
+                          self.params.fields,
+                          self.params.resource.fields),
+                          m_instances)
 
         return count('instances', m_instances)
-
 
     def delete(self, *args, **kwargs):
         """
             Receives a request for destroy a running/requested instance
         """
-        (args, token) = (self.params.delete, kwargs.get('token'))        
+        (args, token) = (self.params.delete, kwargs.get('token'))
 
         instances = Instance.update(state='REMOVED').where(
             Instance.token == kwargs.get('token'))
 
         if args['state']:
             instances = instances.where(Instance.state == args['state'])
-        
+
         instance_ids = args.get('instance_ids', None)
         if instance_ids:
             instance_filters = []
             for instance_id in instance_ids:
                 instance_filters.append(Instance.instance_id == instance_id)
 
-            import operator
-            instances = instances.where(reduce(operator.or_, 
+            instances = instances.where(reduce(operator.or_,
                                                instance_filters))
         affected = instances.execute()
         return affected
-
 
     def post(self, *args, **kwargs):
         """
@@ -141,7 +135,7 @@ class InstanceResource(Resource):
         """
         try:
             instance = Instance.create_from_args(self.params.post, kwargs)
-        except Except as ex:
+        except Exception as ex:
             logger.error(ex.message)
             return abort(500)
 
